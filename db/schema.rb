a25cd04ac0f02136dd812b591d4132fb2f5beda4
Will Skywalker <cxbats@gmail.com>
2019-02-02 12:30:45 +0100

    image url migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_102801) do
+ActiveRecord::Schema.define(version: 2019_02_02_112440) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
 end
